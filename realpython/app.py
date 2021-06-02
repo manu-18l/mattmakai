a01336c7e302e0186c3fb95c236346705c69d2a2
manu-18l <guchun6381@yeah.net>
2021-06-03 03:42:07 +0800

    adding a bit for posting messages to the channel

--- realpython/app.py
+++ realpython/app.py
@@ -21,16 +21,25 @@ def channel_info(channel_id):
     return None
 
 
+def send_message(channel_id, message):
+    slack_client.api_call("chat.postMessage", channel=channel_id,
+                          text=message, username='pythonbot',
+                          icon_emoji=':robot_face:')
+
+
 if __name__ == '__main__':
     channels = list_channels()
     if channels:
         print("Channels: ")
-        for c in channels:
-            print(c['name'] + " (" + c['id'] + ")")
-            detailed_info = channel_info(c['id'])
+        for channel in channels:
+            print(channel['name'] + " (" + channel['id'] + ")")
+            detailed_info = channel_info(channel['id'])
             if detailed_info:
-                print('Latest text from ' + c['name'] + ":")
+                print('Latest text from ' + channel['name'] + ":")
                 print(detailed_info['latest']['text'])
+            if channel['name'] == 'general':
+                send_message(channel['id'], "Hello " +
+                             channel['name'] + "! It worked!")
         print('-----')
     else:
         print("Unable to authenticate.")
