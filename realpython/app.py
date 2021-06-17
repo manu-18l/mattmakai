ef7711957ccdb47d2fff21bbc64e5c5a4bbe8f57
manu-18l <guchun6381@yeah.net>
2021-06-18 01:58:36 +0800

    sync with blog post

--- realpython/app.py
+++ realpython/app.py
@@ -2,7 +2,7 @@ import os
 from slackclient import SlackClient
 
 
-SLACK_TOKEN = os.environ.get('SLACK_TOKEN', None)
+SLACK_TOKEN = os.environ.get('SLACK_TOKEN')
 
 slack_client = SlackClient(SLACK_TOKEN)
 
